b8c433883bcdf254d0528530e27b6827ed7771b0
Saurabh Badhwar <sbsaurabhbadhwar9@gmail.com>
2016-02-23 22:28:44 +0530

    Make the single post page dynamic

--- wp-content/themes/ieee_niec/single.php
+++ wp-content/themes/ieee_niec/single.php
@@ -9,92 +9,28 @@
   </head>
   <body>
   	<div class="container-float">
-
+      <?php while(have_posts()): the_post(); ?>
   		<div class="col-sm-8" style="margin-left:2%;margin-top:2%">
-  			<h1> Event Name</h1><hr>
-  			<a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/new1.png" class="thumbnail img-responsive" style="box-shadow:10px 5px 5px #efefef"></a><hr>
+  			<h1><?php the_title(); ?></h1><hr>
+  			<a href=""><img src="<?php if(has_post_thumbnail()){
+          echo the_post_thumbnail_url();
+        } ?>" class="thumbnail img-responsive" style="box-shadow:10px 5px 5px #efefef"></a><hr>
   			<div class="panel panel-default">
   				<div class="panel-body">
   					<ul class="list-inline" id="date">
-  						<li class="glyphicon glyphicon-time">17thNovember2015</li>
-  						<li class="glyphicon glyphicon-tags">ieeeniec,ieeedelhi</li>
+  						<li class="glyphicon glyphicon-time"><?php echo get_the_date(); ?></li>
+  						<li class="glyphicon glyphicon-tags"><?php echo the_tags(); ?></li>
 
   					</ul>
 
 
   				</div>
   			</div>
-  			<p>With the advent of technology, human life has undergone a substantial change. Apps have become an
-                indispensable part of our lives. Each and every need of our daily routine has been packed into a nutshell
-                called an Application. Thus to help students evolve as an application developer, IEEE Delhi Section organized
-                a two-day Android Application Development Workshop on 7th & 8th November,2015 at Northern India Engineering College.
-            </p>
-            <p><strong>DAY</strong></p>
-            <p><strong>17th November 2015</strong></p>
-            <p>Attended by more than 100 enthusiastic students from various colleges of Delhi and NCR, the session
-                started with an introduction regarding the purpose of the event, followed by the speaker Mr. Tushar
-                Varshney familiarizing the audience with the need of android app development.
-            </p>
-            <p>The learning process began with the students installing the required software packages. Appropriate
-                Directions were given for the installations of software packages supporting different operating systems.
-                Students were taught to make command buttons and uploading background pictures for their apps as a first move.
-            </p>
-            <p>The Speaker encouraged the participants by stating that further explorations in android application
-                development will give better outcomes and thereby one can easily become an expertise in this field.
-                Many students exercised the speaker’s words creating some simple applications which was quite appreciated.
-                The participants were made comfortable as the trained volunteers ensured that they were bereft of any
-                kind of difficulty.
-            </p>
-            <p>A loud cheer and jollity filled the air when a Wheelchair controlled by an android application, built by
-                IEEE-NIEC for old and crippled, entered the auditorium. Tushar Varshney introduced the ‘Sensoduino’ app
-                available on Google Playstore and taught how to interface Arduino UNO to an android application to make
-                it work with Physical objects around. The alacrity of the participants knew no bound as they tested
-                the working of the wheelchair by sitting and controlling it.
-            </p>
-            <p>The idea of making a basic application without sufficient java programming on the very first day of
-                workshop gave immense pleasure to the participants. The day concluded as the participants returned
-                with great zeal and much more aspirations for the ensuing day.
-            </p>
-            <p><strong>DAY 2</strong></p>
-            <p><strong>8th November 2015</strong></p>
-            <p>The second day started with a briefing of the day’s schedule. The participants were warmed up by a
-                revision of previous day’s insights. It was followed by a session dedicated to Web View, Scroll View
-                which is crucial for a developer to access a link over a web page and to scroll the pages in an
-                application respectively. Moreover, the introduction of Splash Screens, insertion of mp3 track in an
-                application was a delight to the audience. Followed by a session on parse.com API Real Time
-                Notifications, being an indispensable part of commercial applications like flipkart, myntra, Ola et al.
-            </p>
-            <p>Volunteers bustled throughout, to meet all the requirements of the participants, making tireless efforts
-                to solve the doubts of the participants.
-            </p>
-            <p>Considering their magnum opus, the participants uploaded the apps designed by them on Google Playstore.
-            </p>
-            <p>This was followed by a guest lecture ‘Life Appified’ by Shamik Ghosh, Business Editor, Telematics Wire ,
-                sharing his acumen and rudiments on the use of android applications and a tremendous bandwagon that it
-                has created in the commercial world. He also gave Statistics related to the dominance of Android development
-                compared to the iOS and other application development platforms.
-            </p>
-            <p>IEEE Awareness session by Vivek Goyal, Chairperson, IEEE-NIEC marked the concluding session of the event,
-                which aimed at disseminating the participants about the benefits of IEEE such as scholarship programs,
-                publications, contests and of that ilk.
-            </p>
-            <p><strong>CONCLUSION</strong></p>
-            <p>IEEE Delhi Section has always impinged upon the technical education for all the right reasons. This time,
-                working in tandem with IEEE-NIEC [Northern India Engineering College], successfully organizing an Android
-                Application Development workshop in the campus of NIEC.
-            </p>
-            <p>An event imbued with the merriment of developing android application and interfacing it with real-world
-                objects was elevated a thousand folds by a jaunty audience, thanked with Refreshment Food in the end which
-                was sponsored by IEEE Delhi Section
-            </p>
-            <p>IEEE-NIEC along with IEEE Delhi Section, did tremendously well in coordinating and training the volunteers
-                as implied by the positive feedbacks given by the participants.
-            </p>
-            <p>IEEE-NIEC looks forward to organize more such technical events in future and will continue to toil towards
-                creating a technical atmosphere.
-            </p>
-
+  			<p>
+          <?php the_content(); ?>
+        </p>
   		</div>
+    <?php endwhile; ?>
   		<div class="col-sm-3" style="background-color:lavenderb;height:800px; margin-left:2%;margin-top:8%">
   			<h3>Recent activites</h3><hr>
   			<div class="row">
