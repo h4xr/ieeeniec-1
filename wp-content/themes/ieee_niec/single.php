cf5213ac0087190e71850c579a67e73e12fefefa
Saurabh Badhwar <sbsaurabhbadhwar9@gmail.com>
2016-02-23 21:53:53 +0530

    Initial support for single blog post(Currently static)

--- wp-content/themes/ieee_niec/single.php
+++ wp-content/themes/ieee_niec/single.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width , intial-scale=1">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/sub_style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/sub_style.css">
     <title>IEEE NIEC</title>
   </head>
   <body>
@@ -12,7 +12,7 @@
 
   		<div class="col-sm-8" style="margin-left:2%;margin-top:2%">
   			<h1> Event Name</h1><hr>
-  			<a href=""><img src="images/new1.png" class="thumbnail img-responsive" style="box-shadow:10px 5px 5px #efefef"></a><hr>
+  			<a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/new1.png" class="thumbnail img-responsive" style="box-shadow:10px 5px 5px #efefef"></a><hr>
   			<div class="panel panel-default">
   				<div class="panel-body">
   					<ul class="list-inline" id="date">
@@ -98,7 +98,7 @@
   		<div class="col-sm-3" style="background-color:lavenderb;height:800px; margin-left:2%;margin-top:8%">
   			<h3>Recent activites</h3><hr>
   			<div class="row">
-  				<div class="col-sm-5"><a href="#"><img src="images/new2.png" class="img-responsive"></a></div>
+  				<div class="col-sm-5"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/new2.png" class="img-responsive"></a></div>
   				<div class="col-sm-7">
   					<p style="font-size:11px;">A workshop on website design was held on 7th, 8th and 9th of September, 2015 by
                         IEEE-NIEC’s prestigious Web Development team
@@ -107,7 +107,7 @@
   			</div>
   			<hr>
   			<div class="row">
-  				<div class="col-sm-5"><a href="#"><img src="images/new2.png" class="img-responsive"></a></div>
+  				<div class="col-sm-5"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/new2.png" class="img-responsive"></a></div>
   				<div class="col-sm-7">
   					<p style="font-size:11px;">A workshop on website design was held on 7th, 8th and 9th of September, 2015 by
                         IEEE-NIEC’s prestigious Web Development team
@@ -116,14 +116,14 @@
   			</div>
   			<hr>
   			<div class="row">
-  				<div class="col-sm-5"><a href="#"><img src="images/new2.png" class="img-responsive"></a></div>
+  				<div class="col-sm-5"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/new2.png" class="img-responsive"></a></div>
   				<div class="col-sm-7">
   					<p style="font-size:11px;">A workshop on website design was held on 7th, 8th and 9th of September, 2015 by
                         IEEE-NIEC’s prestigious Web Development team
                     </p>
   				</div>
   			</div>
-  			
+
   		</div>
 
   	</div>
